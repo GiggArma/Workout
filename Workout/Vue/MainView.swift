24ec0c6b970f217a62e28547860ffb1f22f4c16c
giggarma <hec.brotelande@gmail.com>
2022-03-03 10:42:04 +0100

    Ajout onglet en bas

---
+++ Workout/Vue/MainView.swift
@@ -0,0 +1,35 @@
+//
+//  MainView.swift
+//  Workout
+//
+//  Created by brotelande hector on 03/03/2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            TimerView()
+                .tabItem {
+                    Label("Timer", systemImage: "timer")
+                }
+            
+            WorkoutView()
+                .tabItem {
+                    Label("Workouts", systemImage: "list.bullet")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
