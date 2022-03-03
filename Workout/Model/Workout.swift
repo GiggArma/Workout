8d3afd2c42852983947a77c9edbf70f1812024b8
romainniebel <romainniebel25@gmail.com>
2022-03-03 10:03:07 +0100

    Création du enum bike, run et walk ( Model)

---
+++ Workout/Model/Workout.swift
@@ -0,0 +1,16 @@
+//
+//  Workout.swift
+//  Workout
+//
+//  Created by niebel romain on 03/03/2022.
+//
+
+import Foundation
+import CoreText
+
+enum Mode : String,CaseIterable {
+    case bike
+    case run
+    case walk
+}
+
