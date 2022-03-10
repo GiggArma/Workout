c74948d743c9d0dbf0765faa48213d5f439dd7b6
giggarma <hec.brotelande@gmail.com>
2022-03-10 10:40:01 +0100

    Création chronomètre

--- Workout/Vue/TimerView.swift
+++ Workout/Vue/TimerView.swift
@@ -10,10 +10,39 @@ import SwiftUI
 struct TimerView: View {
     
     @State var mode : Mode = .bike
+    @State var progressTime = 236
+    
+    var hours: Int {
+      progressTime / 3600
+    }
+
+    var minutes: Int {
+      (progressTime % 3600) / 60
+    }
+
+    var seconds: Int {
+      progressTime % 60
+    }
+    
+    @State private var timer: Timer?
     
     var body: some View {
         
         VStack{
+            
+            HStack(spacing: 10) {
+                StopWatchView(timeUnit: hours)
+                    Text(":")
+                        .font(.system(size: 48))
+                        .foregroundColor(.black)
+                        .offset(y: -18)
+                StopWatchView(timeUnit: minutes)
+                    Text(":")
+                        .font(.system(size: 48))
+                        .foregroundColor(.black)
+                        .offset(y: -18)
+                StopWatchView(timeUnit: seconds)
+            }
             
             Picker("Mode",selection: $mode){
                 ForEach(Mode.allCases,id : \.self){pri
@@ -42,9 +71,39 @@ struct TimerView: View {
     }
 }
 
+struct StopWatchView: View {
+    var timeUnit: Int
+    
+    var timeUnitStr: String {
+        let timeUnitStr = String(timeUnit)
+            return timeUnit < 10 ? "0" + timeUnitStr : timeUnitStr
+        }
+
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack(spacing: 2) {
+                    Text(timeUnitStr.substring(index: 0))
+                        .font(.system(size: 48))
+                        .frame(width: 28)
+                    Text(timeUnitStr.substring(index: 1))
+                        .font(.system(size: 48))
+                        .frame(width: 28)
+                }
+            }
+        }
+    }
+}
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
         TimerView()
+    }
+}
+
+extension String {
+    func substring(index: Int) -> String {
+        let arrayString = Array(self)
+        return String(arrayString[index])
     }
 }
