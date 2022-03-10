cf1145829a88ef00e519d53c54648d0f6c176f4f
romainniebel <romainniebel25@gmail.com>
2022-03-10 10:12:54 +0100

    Création First name , Last name , picker ( female , male , other )

--- Workout/Vue/ProfileView.swift
+++ Workout/Vue/ProfileView.swift
@@ -9,19 +9,31 @@ import SwiftUI
 
 struct ProfileView: View {
     
+    @State var sexe : Sexe = .female
     @State var firstName: String = ""
+    @State var lastName : String = ""
     
     var body: some View {
         ZStack {
-            Color("lightGray")
-                .edgesIgnoringSafeArea(.all)
+    
             VStack {
                 TextField("First name", text: $firstName)
                     .padding(.horizontal)
                     .frame(height: 55)
                     .background(Color(.systemGray4))
                     .cornerRadius(10)
-                Spacer()
+                TextField("Last name", text: $lastName)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(.systemGray4))
+                    .cornerRadius(10)
+                Picker("Sexe",selection: $sexe){
+                    ForEach(Sexe.allCases,id : \.self){pri
+                        in
+                        Text("\(pri.rawValue)")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
             }
             .padding(14)
             .navigationTitle("👤 Profile")
